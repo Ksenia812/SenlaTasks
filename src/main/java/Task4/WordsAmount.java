26f0bf0ab5ef509fe52b8c984640cf8571c1aa8f
Ksenia812 <57873750+Ksenia812@users.noreply.github.com>
2020-05-09 14:35:26 +0300

    Find  the number of occurrences of a word in the  input text

---
+++ src/main/java/Task4/WordsAmount.java
@@ -0,0 +1,29 @@
+package Task4;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WordsAmount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input text:");
+        String text = scanner.nextLine();
+        System.out.println("Input the search word:");
+        String searchWord = scanner.nextLine();
+        getAmountOfWords(text, searchWord);
+
+    }
+
+    private static void getAmountOfWords(String input, String searchWord) {
+
+        Predicate<String> isEquals = i -> (i.equals(searchWord.toUpperCase()));
+        int countMatches = (int) Stream.of(input.toUpperCase().split("\\s*(\\s|,|!|\\.|\\?)+\\s*")).
+                filter(isEquals).count();
+        System.out.println(countMatches);
+
+
+    }
+}
